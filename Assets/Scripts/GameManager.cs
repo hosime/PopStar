d2c8da7fb440bf48de7d2e2e1d7d6e72e65293b6
hosime <80332103+hosime@users.noreply.github.com>
2021-03-22 00:22:10 +0800

    点击清除相邻的相同颜色星星

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+    public int MaxRow = 12;
+    public int MaxColumn = 10;
     public GameObject[] StarObjs;
     public GameObject StarGroup;
-    public List<Star> StarList;
-    public int Row = 12;
-    public int Column = 10;
- 
+    public List<Star> StarList;//所有星星
+    public List<Star> ClickedList;//被点击的星星和它相邻的同色星星
+
+    public static GameManager gameManager_Instance;
+
     // Use this for initialization
     void Start () {
+        gameManager_Instance = this;
         CreateStarList();
     }
     
@@ -23,9 +27,9 @@ public class GameManager : MonoBehaviour {
 
     void CreateStarList()
     {
-        for (int r = 0; r < Row; r++)
+        for (int r = 0; r < MaxRow; r++)
         {
-            for (int c = 0; c < Column; c++)
+            for (int c = 0; c < MaxColumn; c++)
             {
                 int index = Random.Range(0, 5);//获取随机颜色星星
                 var obj = Instantiate(StarObjs[index], StarGroup.transform);//实例化星星
@@ -37,5 +41,89 @@ public class GameManager : MonoBehaviour {
                 StarList.Add(star);
             }
         }
+    }
+
+    public void FindTheSameStar(Star currentStar)
+    {
+        int row = currentStar.Row;
+        int column = currentStar.Column;
+        if (row < MaxRow)
+        {
+            foreach (var item in StarList)
+            {
+                if (item.Row == row + 1 && item.Column == column)//判断上方
+                {
+                    if (item.StarColor == currentStar.StarColor)
+                    {
+                        if (!ClickedList.Contains(item))
+                        {
+                            ClickedList.Add(item);
+                            FindTheSameStar(item);
+                        }
+                    }
+                }
+            }
+        }
+        if (row > 0)
+        {
+            foreach (var item in StarList)
+            {
+                if (item.Row == row - 1 && item.Column == column)//判断下方
+                {
+                    if (item.StarColor == currentStar.StarColor)
+                    {
+                        if (!ClickedList.Contains(item))
+                        {
+                            ClickedList.Add(item);
+                            FindTheSameStar(item);
+                        }
+                    }
+                }
+            }
+        }
+        if (column > 0)
+        {
+            foreach (var item in StarList)
+            {
+                if (item.Row == row && item.Column == column - 1)//判断左边
+                {
+                    if (item.StarColor == currentStar.StarColor)
+                    {
+                        if (!ClickedList.Contains(item))
+                        {
+                            ClickedList.Add(item);
+                            FindTheSameStar(item);
+                        }
+                    }
+                }
+            }
+        }
+        if (column < MaxColumn)
+        {
+            foreach (var item in StarList)
+            {
+                if (item.Row == row && item.Column == column + 1)//判断右边
+                {
+                    if (item.StarColor == currentStar.StarColor)
+                    {
+                        if (!ClickedList.Contains(item))
+                        {
+                            ClickedList.Add(item);
+                            FindTheSameStar(item);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public void ClearClickedList()
+    {
+        foreach (var item in ClickedList)
+        {
+            item.DestroyStar();
+            StarList.Remove(item);
+        }
+        ClickedList.Clear();
     }
 }
--- Assets/Scripts/Star.cs
+++ Assets/Scripts/Star.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//星星颜色
+public enum Color { Blue, Green, Orange, Purple, Red }
+
 public class Star : MonoBehaviour {
 
     public int Row = 0;
     public int Column = 0;
+    public Color StarColor = Color.Blue;
 
     // Use this for initialization
     void Start () {
@@ -15,5 +19,17 @@ public class Star : MonoBehaviour {
     // Update is called once per frame
     void Update () {
         
+    }
+
+    public void OnClick_Star()
+    {
+        Debug.Log(StarColor.ToString());
+        GameManager.gameManager_Instance.FindTheSameStar(this);
+        GameManager.gameManager_Instance.ClearClickedList();
+    }
+
+    public void DestroyStar()
+    {
+        Destroy(this.gameObject);
     }
 }
